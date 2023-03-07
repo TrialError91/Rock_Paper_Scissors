90f7e6f590590d2c5aefe06fcd4af85636b546f4
Nicolò De Salvo <125812842+TrialError91@users.noreply.github.com>
2023-03-07 19:20:42 +0100

    Repeat the game for five iterations

--- script.js
+++ script.js
@@ -13,38 +13,40 @@ function checkUserChoice() {
 let userScore = 0
 let computerScore = 0
 
-const choices = ["rock", "paper", "scissors"]
-userChoice=getUserChoice()
-userChoice=userChoice.toLowerCase()
-checkUserChoice()
-while (checkUserChoice()==false) {
+for (let i = 0; i < 5; i++) {
+    const choices = ["rock", "paper", "scissors"]
     userChoice=getUserChoice()
     userChoice=userChoice.toLowerCase()
     checkUserChoice()
-};
+    while (checkUserChoice()==false) {
+        userChoice=getUserChoice()
+        userChoice=userChoice.toLowerCase()
+        checkUserChoice()
+    };
 
-let computerChoice = choices[Math.floor(Math.random()*3)]
+    let computerChoice = choices[Math.floor(Math.random()*3)]
 
-function playRound(userChoice, computerChoice) {
-    if (userChoice=="rock" && computerChoice=="paper") {
-        computerScore += 1
-        return `You chose ${userChoice}. The computer chose ${computerChoice}. You lose this round!`
-    } else if (userChoice=="rock" && computerChoice=="scissors") {
-        userScore += 1
-        return `You chose ${userChoice}. The computer chose ${computerChoice}. You win this round!`
-    } else if (userChoice=="paper" && computerChoice=="rock") {
-        userScore += 1
-        return `You chose ${userChoice}. The computer chose ${computerChoice}. You win this round!`
-    } else if (userChoice=="paper" && computerChoice=="scissors") {
-        computerScore += 1
-        return `You chose ${userChoice}. The computer chose ${computerChoice}. You lose this round!`
-    } else if (userChoice=="scissors" && computerChoice=="rock") {
-        computerScore += 1
-        return `You chose ${userChoice}. The computer chose ${computerChoice}. You lose this round!`
-    } else if (userChoice=="scissors" && computerChoice=="paper") {
-        userScore += 1
-        return `You chose ${userChoice}. The computer chose ${computerChoice}. You win this round!`
-    } else {return `You chose ${userChoice}. The computer chose ${computerChoice}. It's a draw!`}
+    function playRound(userChoice, computerChoice) {
+        if (userChoice=="rock" && computerChoice=="paper") {
+            computerScore += 1
+            return `You chose ${userChoice}. The computer chose ${computerChoice}. You lose this round!`
+        } else if (userChoice=="rock" && computerChoice=="scissors") {
+            userScore += 1
+            return `You chose ${userChoice}. The computer chose ${computerChoice}. You win this round!`
+        } else if (userChoice=="paper" && computerChoice=="rock") {
+            userScore += 1
+            return `You chose ${userChoice}. The computer chose ${computerChoice}. You win this round!`
+        } else if (userChoice=="paper" && computerChoice=="scissors") {
+            computerScore += 1
+            return `You chose ${userChoice}. The computer chose ${computerChoice}. You lose this round!`
+        } else if (userChoice=="scissors" && computerChoice=="rock") {
+            computerScore += 1
+            return `You chose ${userChoice}. The computer chose ${computerChoice}. You lose this round!`
+        } else if (userChoice=="scissors" && computerChoice=="paper") {
+            userScore += 1
+            return `You chose ${userChoice}. The computer chose ${computerChoice}. You win this round!`
+        } else {return `You chose ${userChoice}. The computer chose ${computerChoice}. It's a draw!`}
+    };
+    console.log(playRound(userChoice, computerChoice))
+    console.log(`Computer score: ${computerScore} // User score: ${userScore}`)
 };
-console.log(playRound(userChoice, computerChoice))
-console.log(`Computer score: ${computerScore} // User score: ${userScore}`)
