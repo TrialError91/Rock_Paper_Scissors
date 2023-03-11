1ea1fb0a9f3edea948ae570fc29ea8928265e60e
Nicolò De Salvo <125812842+TrialError91@users.noreply.github.com>
2023-03-11 15:49:47 +0100

    Add a UI to RPS

--- script.js
+++ script.js
@@ -1,64 +1,99 @@
-let userChoice
-let getUserChoice = function() {
-    return prompt("Insert your choice here. Enter a valid input")
-};
-function checkUserChoice() {
-    if ((userChoice=="rock") || (userChoice=="paper") || (userChoice=="scissors")) {
-        return true} 
-    else {
-        return false
-    }
-};
+let userInput
+let computerChoice 
+const choices = ['rock', 'paper', 'scissors']
+const rockBtn = document.querySelector('.rock')
+const paperBtn = document.querySelector('.paper')
+const scisBtn = document.querySelector('.scissors')
+const roundPara = document.querySelector('.round-result')
+const userScorePara = document.querySelector('.user-score')
+const computerScorePara = document.querySelector('.computer-score')
+const matchPara = document.querySelector('.match-result')
 
-let userScore = 0
-let computerScore = 0
+const buttons = document.querySelectorAll('button')
 
-for (let i = 0; i < 5; i++) {
-    const choices = ["rock", "paper", "scissors"]
-    userChoice=getUserChoice()
-    userChoice=userChoice.toLowerCase()
-    checkUserChoice()
-    while (checkUserChoice()==false) {
-        userChoice=getUserChoice()
-        userChoice=userChoice.toLowerCase()
-        checkUserChoice()
-    };
+const container = document.querySelector('.container')
 
-    let computerChoice = choices[Math.floor(Math.random()*3)]
+let playAgain
 
-    function playRound(userChoice, computerChoice) {
-        if (userChoice=="rock" && computerChoice=="paper") {
-            computerScore += 1
-            return `You chose ${userChoice}. The computer chose ${computerChoice}. You lose this round!`
-        } else if (userChoice=="rock" && computerChoice=="scissors") {
-            userScore += 1
-            return `You chose ${userChoice}. The computer chose ${computerChoice}. You win this round!`
-        } else if (userChoice=="paper" && computerChoice=="rock") {
-            userScore += 1
-            return `You chose ${userChoice}. The computer chose ${computerChoice}. You win this round!`
-        } else if (userChoice=="paper" && computerChoice=="scissors") {
-            computerScore += 1
-            return `You chose ${userChoice}. The computer chose ${computerChoice}. You lose this round!`
-        } else if (userChoice=="scissors" && computerChoice=="rock") {
-            computerScore += 1
-            return `You chose ${userChoice}. The computer chose ${computerChoice}. You lose this round!`
-        } else if (userChoice=="scissors" && computerChoice=="paper") {
-            userScore += 1
-            return `You chose ${userChoice}. The computer chose ${computerChoice}. You win this round!`
-        } else {return `You chose ${userChoice}. The computer chose ${computerChoice}. It's a draw!`}
-    };
-    console.log(playRound(userChoice, computerChoice))
-    console.log(`Computer score: ${computerScore} // User score: ${userScore}`)
+let userScore
+let computerScore
+
+userScore=0
+computerScore=0
+
+
+function getUserInput (e) {
+    userInput=e.target.textContent
 };
 
-function compareScores (userScore, computerScore) {
-    if (userScore>computerScore) {
-        console.log("You won!")
-    } else if (userScore<computerScore) {
-        console.log("You lost!")
+function getComputerChoice() {
+    let index = Math.floor(Math.random()*(choices.length))
+    computerChoice = choices[index]
+    console.log(computerChoice)
+};
+
+function compare() {
+    if (computerChoice == userInput) {
+        roundPara.textContent=`You chose ${userInput}, computer chose ${computerChoice}: it's a draw!`
+        console.log(userScore)
+        console.log(computerScore)
+        userScorePara.textContent=`User score: ${userScore}`
+        computerScorePara.textContent=`Computer score: ${computerScore}`
+    } else if ((computerChoice == 'rock' && userInput == 'paper') || (computerChoice == 'paper' && userInput == 'scissors') || (computerChoice == 'scissors' && userInput == 'rock')) {
+        userScore += 1
+        roundPara.textContent=`You chose ${userInput}, computer chose ${computerChoice}: you win this round!`
+        console.log(userScore)
+        console.log(computerScore)
+        userScorePara.textContent=`User score: ${userScore}`
+        computerScorePara.textContent=`Computer score: ${computerScore}`
     } else {
-        console.log("It's a draw")
+        computerScore += 1
+        roundPara.textContent=`You chose ${userInput}, computer chose ${computerChoice}: the computer wins this round!`
+        console.log(userScore)
+        console.log(computerScore)
+        userScorePara.textContent=`User score: ${userScore}`
+        computerScorePara.textContent=`Computer score: ${computerScore}`
     }
-}
+};
 
-compareScores(userScore, computerScore)
+function playRound () {
+    getComputerChoice()
+    compare()
+    if (computerScore==5) {
+        matchPara.textContent=`Game over! Computer wins the match!`
+        buttons.forEach((button) => button.disabled=true)
+        playAgain=document.createElement('button')
+        container.appendChild(playAgain)
+        playAgain.textContent='Play again'
+        playAgain.addEventListener('click', function (e) {
+            computerScore=0
+            userScore=0
+            userScorePara.textContent=''
+            computerScorePara.textContent=''
+            roundPara.textContent=''
+            matchPara.textContent=''
+            container.removeChild(playAgain)
+            buttons.forEach((button)=>button.disabled=false)
+        });
+    } else if (userScore==5) {
+        matchPara.textContent=`Game over! Congratulations, you have won the match!`
+        buttons.forEach((button) => button.disabled=true)
+        playAgain=document.createElement('button')
+        container.appendChild(playAgain)
+        playAgain.textContent='Play again'
+        playAgain.addEventListener('click', function (e) {
+            computerScore=0
+            userScore=0
+            userScorePara.textContent=''
+            computerScorePara.textContent=''
+            roundPara.textContent=''
+            matchPara.textContent=''
+            container.removeChild(playAgain)
+            buttons.forEach((button)=>button.disabled=false)
+        });
+    };
+};
+
+
+buttons.forEach((button) => button.addEventListener('click', getUserInput, true))
+buttons.forEach((button) => button.addEventListener('click', playRound, true))
